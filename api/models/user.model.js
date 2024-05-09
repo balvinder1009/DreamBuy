d7cb41a23e1e2fba445bad3795de817a6023bc51
balvinder <parwarbalvinder@gmail.com>
2024-05-09 12:21:00 +0530

    create signup api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -12,7 +12,7 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
-    password: {
+    hashedPassword: {
       type: String,
       required: true,
     },
